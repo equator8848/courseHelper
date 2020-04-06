a4a2ca323214efea203ae6a552bcbc35baf53c71
LIBINKAI <841116931@qq.com>
2020-04-06 14:30:13 +0800

    测试

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ git commit -m "$1"
 
 git push origin master
 
-# ssh -i X:/configuration/server_key/key/root  root@132.232.2.232 "cd /data/courseHelper/ ; sh release.sh"
+ssh -i X:/configuration/server_key/key/root  root@132.232.2.232 "cd /data/courseHelper/ ; sh release.sh"
