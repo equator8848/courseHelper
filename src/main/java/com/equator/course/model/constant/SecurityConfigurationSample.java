3bd7eb8ebf81b8e2172efc8649a714a8581bba69
LIBINKAI <841116931@qq.com>
2020-05-02 07:38:34 +0800

    readme

--- src/main/java/com/equator/course/model/constant/SecurityConfigurationSample.java
+++ src/main/java/com/equator/course/model/constant/SecurityConfigurationSample.java
@@ -1,6 +1,9 @@
 package com.equator.course.model.constant;
 
 /**
+ * 实际上有一个SecurityConfiguration类保存了敏感信息，但是被gitignore了
+ * 此SecurityConfigurationSample为样本
+ *
  * @Author: Equator
  * @Date: 2020/4/7 15:54
  **/
